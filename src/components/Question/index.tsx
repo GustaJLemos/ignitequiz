af50e76759d13fc2612be30f6eea8f734fe5d980
Gustavo José Lemos Mendes <gustavo.mendes@w-security.intranet>
2023-07-07 12:48:56 -0300

    Criando nossas próprias animações

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,7 +1,8 @@
-import { View, Text } from 'react-native';
+import { View, Text, Dimensions } from 'react-native';
 
 import { Option } from '../Option';
 import { styles } from './styles';
+import Animated, { Keyframe } from 'react-native-reanimated';
 
 type QuestionProps = {
   title: string;
@@ -14,9 +15,66 @@ type Props = {
   setAlternativeSelected?: (value: number) => void;
 }
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+// Vamos utilizar keyframes para construir uma animação nossa do zero
+
 export function Question({ question, alternativeSelected, setAlternativeSelected }: Props) {
+  // useWindowDimensions é um hook que fica observando se houve alguma mudança no tamanho da tela, por ex, mudamos na horizontal
+
+  // Assim q criamos nossa animação customizada através do keyframe
+  // frame a frame oq a animação deve fazer
+  const enteringKeyframe = new Keyframe({
+    // aq definimos em qual passo estamos e oq fazemos, podemos definir com um numero, uma porcentagem, uma string
+    // dizemos oq nossa animação vai fazer em cada frame
+    // essa estratégia é bem legal pra quando vc quer definir exatamente oq sua animação deve fazer exatamente nesse frama
+    0: {
+      opacity: 0,
+      transform: [
+        // jogando nosso componente pra fora da tela
+        { translateX: SCREEN_WIDTH },
+        { rotate: '90deg' }
+      ]
+    },
+    70: {
+      opacity: 0.3
+    },
+    100: {
+      opacity: 0,
+      transform: [
+        // jogando nosso componente pra fora da tela
+        { translateX: 0 },
+        { rotate: '0deg' }
+      ]
+    }
+  });
+
+  // aq simplesmente sai disso e vai pra isso
+  const exitingKeyframe = new Keyframe({
+    // aq vamos fazer um keyframe de um jeito mais enxuto
+    from: {
+      opacity: 1,
+      transform: [
+        { translateX: 0 },
+        { rotate: '0deg' }
+      ]
+    },
+    to: {
+      opacity: 0,
+      transform: [
+        { translateX: SCREEN_WIDTH * (-1) },
+        { rotate: '-90deg' }
+      ]
+    }
+  });
+
   return (
-    <View style={styles.container}>
+    <Animated.View
+      style={styles.container}
+      // podemos também adicionar modificadores nos nosso keyframes
+      entering={enteringKeyframe.duration(400)}
+      exiting={exitingKeyframe.duration(400)}
+    >
       <Text style={styles.title}>
         {question.title}
       </Text>
@@ -31,6 +89,6 @@ export function Question({ question, alternativeSelected, setAlternativeSelected
           />
         ))
       }
-    </View>
+    </Animated.View>
   );
 }
